0e112fe954cffa6f67cc6bd2a83b8eba8482e059
oleg-golos <oleg_golos@outlook>
2020-09-01 12:52:38 +0300

    add the script and readme files

---
+++ README.md
@@ -0,0 +1,2 @@
+# DNSBL Checker
+The script checks the specified IPs against Spamhaus DNSBL DB via standard DNS protocol and sends alert to the specified addresses in case any of the IPs have been blacklisted. The script should be run periodically via scheduler.
---
+++ dnsbl_checker.ps1
@@ -0,0 +1,33 @@
+﻿#SMTP server used to send alerts
+$PSEmailServer = "mailserver.domain.com"
+
+#External IPs to check
+$ips = @("11.22.33.44","55.66.77.88")
+#Notification recipients
+$receps = @("admin1@domain.com","admin2@domain.com")
+
+#The following function converts IP address to DNSBL name by reversing IP octets and adding them to the ".pbl.spamhaus.org" domain.
+function ip_to_dnsbl_name
+    {
+        param ($ip)
+        $ip = $ip.split(".")
+        [array]::Reverse($ip)
+        $ip = $ip -join "."
+        $ip = $ip + ".pbl.spamhaus.org"
+        $ip
+     }
+#The following function checks converted IP address agains Spamhaus DNSBL
+function blacklist_check
+    {
+        param ($ips)
+        foreach ($ip in $ips)
+        {
+            $dnsbl_name = ip_to_dnsbl_name($ip)
+            if (Resolve-DnsName -DnsOnly $dnsbl_name)
+            {
+                Send-MailMessage -to $receps -from "dnsbl_checker@domain.com" -subject "$ip is blacklisted" -body "$ip has been blacklisted by Spamhaus.org`n https://www.spamhaus.org/query/ip/$ip"
+             }
+         }
+     }       
+
+blacklist_check $ips
